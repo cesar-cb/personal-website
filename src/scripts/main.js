347a0decb6c351c8211e3ec9ce3576dc4ba8a3bd
Cesar Boaventura <cesar.boaventura61@gmail.com>
2020-03-02 15:39:38 -0300

    chore: rearrange js files

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,3 @@
-import { diffYears } from './utils/date';
-
-const birth = new Date('May 11, 1997');
-const jobStart = new Date('March 15, 2016');
-
-const now = new Date();
-
-const getAge = diffYears(now, birth);
-const getExperience = diffYears(now, jobStart);
-
-const ageElement = document.querySelector('#age');
-const experienceElement = document.querySelector('#experience');
-
-ageElement.innerHTML = getAge;
-experienceElement.innerHTML = getExperience;
+import './ga';
+import './particles';
+import './dynamicData';
