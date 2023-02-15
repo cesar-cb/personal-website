a80a366ddfe44fddcbd15a914d647a1250d09690
Cesar Boaventura <cesar.boaventura61@gmail.com>
2023-02-14 23:13:15 -0300

    feat: Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,37 +1,64 @@
-import { FC, ReactNode } from 'react'
+import { AnchorHTMLAttributes, ButtonHTMLAttributes, FC } from 'react'
 import classNames from 'classnames'
+import Link from 'next/link'
+import { TAnchorElementProps } from 'components/Anchor/types'
+import { splitAnchorLinkProps } from 'components/Anchor/utils'
 import styles from './Button.module.scss'
 
+type TButtonAttributes = ButtonHTMLAttributes<HTMLButtonElement>
+type TAnchorAttributes = AnchorHTMLAttributes<HTMLAnchorElement>
+
+type TConditionalProps =
+  | ({ element?: 'a' } & TAnchorAttributes)
+  | ({ element?: 'button' } & TButtonAttributes)
+
 export type TProps = {
-  children: ReactNode
   outline?: boolean
   arrow?: boolean
-  asLink?: boolean
   className?: string
-}
+} & TConditionalProps
 
 const Button: FC<TProps> = ({
   children,
   className,
   outline = false,
   arrow = false,
-  asLink = false,
+  element,
   ...rest
 }) => {
-  const Component = asLink ? 'a' : 'button'
-
   const buttonClasses = classNames(styles.button, className, {
     [styles.outline]: outline,
   })
 
+  const getContent = () => (
+    <span>
+      {children}
+      {arrow && <span>→</span>}
+    </span>
+  )
+
+  if (element === 'a') {
+    const { linkProps, anchorProps } = splitAnchorLinkProps({
+      ...(rest as TAnchorElementProps),
+      className: buttonClasses,
+    })
+
+    return (
+      <Link {...linkProps} {...anchorProps}>
+        {getContent()}
+      </Link>
+    )
+  }
+
+  const buttonProps = {
+    ...(rest as TButtonAttributes),
+    className: buttonClasses,
+  }
+
   return (
-    <Component
-      {...rest}
-      type={asLink ? undefined : 'button'}
-      className={buttonClasses}
-    >
-      {children} {arrow && <span>→</span>}
-    </Component>
+    <button type="button" {...buttonProps}>
+      {getContent()}
+    </button>
   )
 }
 
