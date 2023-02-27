2b1b60fd41a594724e27f226d50d4cd4f7ab7c2a
Cesar Boaventura <cesar.boaventura61@gmail.com>
2023-02-27 13:18:27 -0300

    feat: posts page

--- src/components/PostPreview/PostPreview.tsx
+++ src/components/PostPreview/PostPreview.tsx
@@ -1,38 +1,55 @@
 import { FC } from 'react'
 import Typography from 'components/Typography'
-import Anchor from 'components/Anchor'
-import NextLink from 'next/link'
 import { BiTag } from 'react-icons/bi'
 import styles from './PostPreview.module.scss'
+import classNames from 'classnames'
+import { pluralize } from 'lib/text'
 
 type TProps = {
   title: string
   description: string
   tags: Array<string>
-  color?: 'primary' | 'secondary'
-  className?: string
-  link: {
-    href: string
-  } & typeof NextLink.defaultProps
+  type: 'article' | 'project'
+  date: string
+  readingTime: number
 }
 
 const SectionTitle: FC<TProps> = ({
   title,
   description,
   tags,
-  className = '',
-  link: { href, ...restLink },
+  type,
+  date,
+  readingTime,
 }) => {
+  // const tagClasses = classNames(styles.tag, {
+  //   [styles.articleTag]: type === 'article',
+  //   [styles.projectTag]: type === 'project',
+  // })
+
   return (
-    <Anchor href={href} hideArrow className={className} {...restLink}>
-      <Typography variant="h3" as="h4" className={styles.title}>
+    <article className={styles.container}>
+      <Typography variant="h4" as="h4" className={styles.title}>
         {title}
       </Typography>
+      <time className={styles.date}>
+        <Typography as="p" variant="label">
+          {new Intl.DateTimeFormat('pt-BR', { dateStyle: 'long' }).format(
+            new Date(date),
+          )}{' '}
+          • {Math.ceil(readingTime)} min de leitura
+        </Typography>
+      </time>
       <Typography className={styles.description}>{description}</Typography>
+      {/* <span className={tagClasses}>{type}</span> */}
       <span className={styles.tags}>
-        <BiTag className={styles.tagIcon} size={15} /> {tags.join(', ')}
+        <BiTag className={styles.tagIcon} size={15} />{' '}
+        <Typography as="p" variant="label">
+          {tags.join(', ')}
+        </Typography>
       </span>
-    </Anchor>
+      <div className={styles.hover} />
+    </article>
   )
 }
 
---
+++ src/pages/posts/index.page.tsx
@@ -0,0 +1,63 @@
+import { NextPage } from 'next'
+import Anchor from 'components/Anchor'
+import Box from 'components/Box'
+import PostPreview from 'components/PostPreview'
+import DefaultLayout from 'layouts/DefaultLayout'
+import { getAllPublished } from 'lib/notion'
+import styles from './Posts.module.scss'
+import readingTime from 'reading-time'
+
+type TPost = {
+  id: string
+  title: string
+  tags: Array<string>
+  description: string
+  date: string
+  slug: string
+  type: 'project' | 'article'
+  readingTime: number
+}
+
+type TPageProps = {
+  posts: Array<TPost>
+}
+
+const Posts: NextPage<TPageProps> = ({ posts }) => {
+  return (
+    <DefaultLayout small>
+      <div className={styles.postsContainer}>
+        {posts.map((post) => (
+          <Anchor
+            href={`/posts/${post.slug}`}
+            hideArrow
+            className={styles.boxWrapper}
+            key={post.id}
+          >
+            <PostPreview
+              key={post.id}
+              title={post.title}
+              description={post.description}
+              tags={post.tags}
+              type={post.type}
+              date={post.date}
+              readingTime={post.readingTime}
+            />
+          </Anchor>
+        ))}
+      </div>
+    </DefaultLayout>
+  )
+}
+
+export const getStaticProps = async () => {
+  const data = await getAllPublished()
+
+  return {
+    props: {
+      posts: data,
+    },
+    revalidate: 60,
+  }
+}
+
+export default Posts
